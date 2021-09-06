8183250cb20ddecc0b6dc37489f3f604c7c9fa62
mattbrannon <sh4rkbot@gmail.com>
2021-09-06 07:58:02 -0500

    add global styles

---
+++ pages/_document.js
@@ -0,0 +1,61 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          // eslint-disable-next-line react/display-name
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {' '}
+            {initialProps.styles} {sheet.getStyleElement()}{' '}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/futura/futura-medium-01-webfont.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="true"
+          />
+          <link
+            rel="preload"
+            href="https://pixelambacht.nl/remote/RobotoFlex[slnt,wdth,wght,opsz].woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="true"
+          />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />{' '}
+          <link
+            href="https://fonts.googleapis.com/css2?family=Coming+Soon&family=Oswald&family=Amatic+SC:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
---
+++ styles/global.js
@@ -0,0 +1,251 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'futura';
+    src: url('/fonts/futura/futura-bold-03-webfont.woff2') format('woff2'),
+      url('/fonts/futura/futura-bold-03-webfont.woff') format('woff');
+    font-weight: 700;
+    font-style: normal;
+    font-display: swap;
+  }
+
+  @font-face {
+    font-family: 'futura narrow';
+    src: url('/fonts/futura/futura-condensedextrabold-05-webfont.woff2') format('woff2'),
+      url('/fonts/futura/futura-condensedextrabold-05-webfont.woff') format('woff');
+    font-weight: 900;
+    font-style: normal;
+    font-display: swap;
+
+  }
+
+  @font-face {
+    font-family: 'futura narrow';
+    src: url('/fonts/futura/futura-condensedmedium-04-webfont.woff2') format('woff2'),
+      url('/fonts/futura/futura-condensedmedium-04-webfont.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+
+  }
+
+  @font-face {
+    font-family: 'futura';
+    src: url('/fonts/futura/futura-medium-01-webfont.woff2') format('woff2'),
+      url('/fonts/futura/futura-medium-01-webfont.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+
+  }
+
+  @font-face {
+    font-family: 'futura';
+    src: url('/fonts/futura/futura-mediumitalic-02-webfont.woff2') format('woff2'),
+      url('/fonts/futura/futura-mediumitalic-02-webfont.woff') format('woff');
+    font-weight: normal;
+    font-style: italic;
+    font-display: swap;
+
+  }
+
+  @font-face {
+    font-family: 'Roboto Flex';
+    src: url('https://pixelambacht.nl/remote/RobotoFlex[slnt,wdth,wght,opsz].woff2')
+        format('woff2 supports variations'),
+      url('https://pixelambacht.nl/remote/RobotoFlex[slnt,wdth,wght,opsz].woff2')
+        format('woff2-variations');
+    font-weight: 100 1000;
+    font-stretch: 25% 300%;
+    font-display: swap;
+
+  }
+
+  @font-face {
+    font-family: 'bloody';
+    src: url('/fonts/bloody/bloody-webfont.woff2') format('woff2'),
+      url('/fonts/bloody/bloody-webfont.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+
+  }
+
+  :root {
+    --headerHeight: 80px;
+    --footerHeight: 70px;
+    --mainHeight: calc(100% - (var(--headerHeight) - var(--footerHeight)));
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    list-style-type: none;
+    text-decoration: none;
+    margin: 0;
+    padding: 0;
+    /* font-family: futura; */
+  }
+
+  /* Remove default margin */
+
+  /* Set core root defaults */
+  html:focus-within {
+    scroll-behavior: smooth;
+  }
+
+  /* Set core body defaults */
+  body {
+    min-height: 100vh;
+    text-rendering: optimizeSpeed;
+    /* background: hsl(208, 100%, 97%); */
+    background: #f1f3f8;
+    line-height: 1.6;
+    font-size: 1.1rem;
+    /* isolation: isolate; */
+  }
+
+
+  #__next {
+    min-height: inherit;
+    display: grid;
+    grid-template-areas: 
+    'header'
+    'main'
+    'footer';
+
+    grid-template-columns: repeat(auto-fill, minmax(min(100vw, 100%), 1fr));
+    grid-template-rows: 80px calc(100% - 180px) 100px;
+  }
+
+
+  main {
+    padding-top: 32px;
+  }
+
+
+
+  a:hover {
+    text-decoration: revert;
+  }
+
+  /* Make images easier to work with */
+  /* img,
+  picture {
+    height: auto;
+    max-width: 100%;
+    display: block;
+    object-fit: cover;
+  } */
+
+  /* Inherit fonts for inputs and buttons */
+  /* input,
+  button,
+  textarea,
+  select {
+    font: inherit;
+  } */
+
+  p,
+  ul,
+  li {
+    margin: revert;
+    padding: revert;
+  }
+
+  /* width */
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  /* Track */
+  ::-webkit-scrollbar-track {
+    /* background: linear-gradient(180deg, #333 0px, #333 80px, transparent 80px);
+    background: hsl(195, 53%, 79%); */
+    background: hsla(0deg, 0%, 80%, 0.9);
+  }
+
+  /* Handle */
+  ::-webkit-scrollbar-thumb {
+    background: hsl(215, 23%, 45%);
+    border: 2px ridge hsl(215, 20%, 34%);
+
+    border-radius: 6px;
+  }
+
+  /* Handle on hover */
+  ::-webkit-scrollbar-thumb:hover {
+    background: hsl(215, 33%, 52%);
+  }
+
+  a,h1,h2,h3,h4,h5,h6 {
+    font-family: 'futura', system-ui, sans-serif;
+  }
+
+  p,li, section {
+    font-family: 'Roboto Flex', system-ui, sans-serif;
+    line-height: 1.6;
+  }
+
+  button {
+    font-family: 'futura';
+  }
+
+
+  a:focus,
+  button:focus {
+    outline: revert;
+    outline-offset: 2px;
+  }
+
+  a:focus:not(::focus-visible),
+  button:focus:not(:focus-visible) {
+    outline: none;
+  }
+
+  a:focus-visible,
+  button:focus-visible {
+    outline: 2px solid deepskyblue;
+    outline-offset: 0.3em;
+  }
+
+  pre > *{
+    font-size: 0.9rem !important;
+  }
+
+  /* Remove all animations, transitions and smooth scroll for people that prefer not to see them */
+  @media (prefers-reduced-motion: reduce) {
+    html:focus-within {
+      scroll-behavior: auto;
+    }
+
+    *,
+    ::before,
+    ::after {
+      animation-delay: -1ms !important;
+      animation-duration: 1ms !important;
+      animation-iteration-count: 1 !important;
+      background-attachment: initial !important;
+      scroll-behavior: auto !important;
+      transition-duration: 0s !important;
+      transition-delay: 0s !important;
+    }
+  }
+
+  @media (prefers-color-scheme: dark) {
+    body {
+      background: #333;
+      color: beige;
+    }
+
+    a {
+      color: hsl(195, 53%, 79%);
+    }
+
+    a:visited {
+      color: hsl(245, 93%, 79%);
+    }
+  }
+`;
