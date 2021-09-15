115d0da299d710dac53c63af9e3e07b8bfdd392d
mattbrannon <sh4rkbot@gmail.com>
2021-09-14 23:14:08 -0500

    updatee utils to only return articles labeled as unpublished

--- utils/mdxUtils.js
+++ utils/mdxUtils.js
@@ -1,19 +1,20 @@
 import fs from 'fs';
 import path from 'path';
+import matter from 'gray-matter';
 
 // POSTS_PATH is useful when you want to get the path to a specific file
 export const POSTS_PATH = path.join(process.cwd(), 'articles');
 
 // postFilePaths is the list of all mdx files inside the POSTS_PATH directory
-export const postFilePaths = fs
+export const allFilePaths = fs
   .readdirSync(POSTS_PATH)
   // Only include md(x) files
   .filter((path) => /\.mdx?$/.test(path));
 
-// const APPS_PATH = path.join(process.cwd(), '/pages/apps');
+export const postFilePaths = allFilePaths.filter((filename) => {
+  const fullPath = path.resolve(POSTS_PATH, filename);
+  const source = fs.readFileSync(fullPath);
+  const { data } = matter(source);
 
-// export const appsFilePaths = fs.readdirSync(APPS_PATH).filter((filepath) => {
-//   return /\.js?$/.test(filepath) && filepath !== 'index.js';
-// });
-
-// console.log(appsFilePaths);
+  return data.status !== 'unpublished';
+});
