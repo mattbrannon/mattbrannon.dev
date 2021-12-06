9cf23379ca9648bd8d6255093955486fc764da87
mattbrannon <sh4rkbot@gmail.com>
2021-12-06 03:10:11 -0600

    use Recursive variable font. update colors. add animation

--- components/FancyText.js
+++ components/FancyText.js
@@ -1,11 +1,27 @@
-import styled from 'styled-components/macro';
+import styled, { keyframes } from 'styled-components/macro';
+
+const slantText = keyframes`
+  from {
+    color: var(--original);
+    font-variation-settings: var(--recursive6);
+  }
+  to {
+    color: var(--color);
+    font-variation-settings: var(--recursive2);
+  }
+`;
 
 const FancyText = styled.span`
-  display: inline;
-  font-family: 'Coming Soon';
-  color: deeppink;
+  font-family: Recursive, 'Coming Soon', sans-serif;
+  font-variation-settings: var(--recursive2);
+  --original: black;
+  --color: var(--pinkBg);
+  color: var(--color);
+  animation: ${slantText} 1000ms ease both 1000ms;
   @media (prefers-color-scheme: dark) {
-    color: deepskyblue;
+    --original: beige;
+    --color: var(--blue-main);
+    color: var(--color);
     font-weight: 700;
   }
 `;
