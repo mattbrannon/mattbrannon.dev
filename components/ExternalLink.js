e2d78f99ead90ed759683bc8afb98c3e0aa93301
mattbrannon <sh4rkbot@gmail.com>
2021-12-06 03:23:43 -0600

    add InternalLink component and update colors

--- components/ExternalLink.js
+++ components/ExternalLink.js
@@ -1,12 +1,38 @@
 import { Anchor } from './Anchor';
 import Link from 'next/link';
+import styled from 'styled-components/macro';
 
 export const ExternalLink = ({ href, children }) => {
   return (
     <Link passHref href={href}>
-      <Anchor newTab href={href}>
+      <A newTab href={href}>
         {children}
-      </Anchor>
+      </A>
     </Link>
   );
 };
+
+export const InternalLink = ({ href, children }) => {
+  return (
+    <Link passHref href={href}>
+      <A href={href}>{children}</A>
+    </Link>
+  );
+};
+
+const A = styled(Anchor)`
+  --link-color: deeppink;
+  font-weight: 700;
+  color: var(--link-color);
+
+  &:hover {
+    --link-color: var(--pinkShadow);
+  }
+
+  @media (prefers-color-scheme: dark) {
+    --link-color: var(--blue1);
+    &:hover {
+      color: var(--blue2);
+    }
+  }
+`;
