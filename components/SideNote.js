d0e6cf3c0238a320756a8c9a5a91142dacacf5a6
mattbrannon <sh4rkbot@gmail.com>
2021-12-06 03:01:18 -0600

    add top and bottom margin. change font family to Open Sans

--- components/SideNote.js
+++ components/SideNote.js
@@ -5,11 +5,13 @@ export default function SideNote({ children }) {
 }
 
 const AsideWrapper = styled.aside`
+  display: block;
+  margin: 1em 0;
   background: palegoldenrod;
   padding: 16px;
   border-top-right-radius: 8px;
   border-bottom-right-radius: 8px;
-  font-family: 'Roboto Flex';
+  font-family: 'Open Sans', system-ui, sans-serif;
   border-left: 6px solid gold;
   font-size: 1rem;
 
