3ea504ae59a6ee29f0184196e7052d1df96239fc
mattbrannon <sh4rkbot@gmail.com>
2021-12-10 23:06:58 -0600

    add contact page

---
+++ pages/contact/index.js
@@ -0,0 +1,22 @@
+import { TopRow } from '@components/Layout';
+import PageTitle from '@components/PageTitle';
+import ContactForm from '@components/ContactForm';
+
+{
+  /* <DocumentHead title="Contact" desc="If you want to get in touch, here's how.." /> */
+}
+
+export default function Contact() {
+  return (
+    <section id="contact">
+      <TopRow>
+        <PageTitle>Contact Me</PageTitle>
+      </TopRow>
+      <p>
+        Wanna get in touch? Send me a message using the form below. All messages go staight to my
+        inbox.
+      </p>
+      <ContactForm />
+    </section>
+  );
+}
