f14ceb4ff199ae51a90f2e80c47f762ba32f02dd
mattbrannon <sh4rkbot@gmail.com>
2021-12-10 18:07:59 -0600

    add constants file

---
+++ constants/index.js
@@ -0,0 +1,24 @@
+export const springDown = 'cubic-bezier(0.17, 0.88, 0.32, 1.27)';
+export const springUp = 'cubic-bezier(0.47, 1.74, 0.99, 0.99)';
+
+export const fonts = {
+  families: {
+    links: 'Jost',
+    paragraphs: 'Open Sans',
+    headings: 'Recursive',
+  },
+  sizes: {
+    size10: `${10 / 16}rem`,
+    size12: `${12 / 16}rem`,
+    size14: `${14 / 16}rem`,
+    size16: `${16 / 16}rem`,
+    size18: `${18 / 16}rem`,
+    size20: `${20 / 16}rem`,
+    size21: `${21 / 16}rem`,
+    size24: `${24 / 16}rem`,
+    size28: `${28 / 16}rem`,
+    size32: `${32 / 16}rem`,
+    size40: `${40 / 16}rem`,
+    size48: `${48 / 16}rem`,
+  },
+};
