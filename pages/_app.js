50fbc2eef6cf2608f98dcda71a2faa7b1f288e09
mattbrannon <sh4rkbot@gmail.com>
2022-04-03 18:56:59 -0500

    update _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,65 +1,32 @@
 import Footer from '@components/Footer';
-import Header, { MobileMenu } from '@components/Header';
-import { usePathname } from '@hooks/usePathname';
-import { useCssVariable } from '@hooks/useCssVariable';
-import { FontSizes } from '@styles/index';
-import '@styles/global.css';
-import { MotionConfig } from 'framer-motion';
-import isValidProp from '@emotion/is-prop-valid';
+import Header from '@components/Header';
 import Layout, { Main } from '@components/Layout';
+import { breakpoints } from '@constants/breakpoints';
+import isValidProp from '@emotion/is-prop-valid';
+import { useMediaQuery } from '@hooks/useMediaQuery';
+import { usePathname } from '@hooks/usePathname';
+import { useScroll } from '@hooks/useScroll';
+// import { loadFeatures } from '@utils/helpers';
 
-import { useEffect, useState, useRef } from 'react';
-import { ThemeProvider } from 'styled-components';
-
-const HEADER_SCROLL_THRESHOLD = 400;
-
-// export const debounce = (func, wait) => {
-//   let timeout;
-//   return function (...args) {
-//     const context = this;
-//     if (timeout) clearTimeout(timeout);
-//     timeout = setTimeout(() => {
-//       timeout = null;
-//       func.apply(context, args);
-//     }, wait);
-//   };
-// };
-
-// const throttle = (callback, limit) => {
-//   let isThrottled;
-//   return function (args) {
-//     let context = this;
-//     if (!isThrottled) {
-//       callback.apply(context, ...args);
-//       isThrottled = true;
-//       setTimeout(() => (isThrottled = false), limit);
-//     }
-//   };
-// };
-
-// function debounce(func, timeout = 300) {
-//   let timer;
-//   return (...args) => {
-//     clearTimeout(timer);
-//     timer = setTimeout(() => {
-//       func.apply(this, args);
-//     }, timeout);
-//   };
-// }
+import '@styles/global.css';
+import { GlobalStyle } from '@styles/index';
+import { MotionConfig } from 'framer-motion';
+import { ThemeProvider } from 'next-themes';
+import { useEffect, useRef, useState } from 'react';
+import { ThemeProvider as ContextProvider, StyleSheetManager } from 'styled-components';
 
 export default function Application({ Component, pageProps }) {
   const [ isOpen, setIsOpen ] = useState(null);
-  const [ hasRun, setHasRun ] = useState(false);
-  const [ isPlaying, setIsPlaying ] = useState(null);
-  const [ hasPlayed, setHasPlayed ] = useState(false);
-  const [ showImage, setShowImage ] = useState(false);
+  const [ hasRun, setHasRun ] = useState(null);
+  const [ isVisible, setIsVisible ] = useState(true);
+  const isMobile = useMediaQuery({ maxWidth: breakpoints.mobile });
+  const scroll = useScroll();
+
+  const HEADER_SCROLL_THRESHOLD = 200;
+
   const pathname = usePathname();
-  const [ currentPath, setCurrentPath ] = useState(pathname);
-  const [ pageButtonPressed, setPageButtonPressed ] = useState(false);
   const ref = useRef();
-  // const [ isHeaderVisible, setIsHeaderVisible ] = useState(true);
-  // const [ headerPosition, setHeaderPosition ] = useCssVariable('--header-position');
-  // const setHeaderPosition = useCssVariable('--header-position', 'translateY(0%)');
+  const header = useRef();
 
   const theme = {
     isOpen,
@@ -67,152 +34,63 @@ export default function Application({ Component, pageProps }) {
     hasRun,
     setHasRun,
     pathname,
-    isPlaying,
-    setIsPlaying,
-    hasPlayed,
-    setHasPlayed,
-    showImage,
-    setShowImage,
-    pageButtonPressed,
-    setPageButtonPressed,
   };
 
   useEffect(() => {
-    const root = document.querySelector('html');
+    const root = document.querySelector('body');
     if (isOpen) {
       root.style.setProperty('overflow', 'hidden');
+      root.style.setProperty('position', 'fixed');
+      root.style.setProperty('background', 'hsla(220deg, 35%, 6%, 0.93)');
     }
     else {
-      root.style.setProperty('overflow', 'auto');
+      root.style.removeProperty('overflow');
+      root.style.removeProperty('position');
+      root.style.setProperty('background', 'var(--body-background)');
+
+      window.scrollTo({ top: scroll.previous, left: 0, behavior: 'auto' });
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [ isOpen ]);
 
-  // useEffect(() => {
-  //   setCurrentPath(pathname);
-  //   const rootStyle = document.querySelector('html').style;
-  //   const isHidden = rootStyle.getPropertyValue('overflow') === 'initial';
-  //   const shouldBeHidden = !hasRun && pathname === '/';
-  //   if (isHidden && !shouldBeHidden) {
-  //     rootStyle.setProperty('overflow', 'auto');
-  //   }
-  // }, [ pathname, hasRun ]);
+  useEffect(() => {
+    let isVisible;
+    if (!isMobile) {
+      const { previous, current } = scroll;
+      const direction = previous < current ? 'down' : 'up';
 
-  // useEffect(() => {
-  //   const root = document.querySelector('#__next');
-  //   function preventDefault(e) {
-  //     e.preventDefault();
-  //   }
+      if (direction === 'down' && current > HEADER_SCROLL_THRESHOLD) {
+        isVisible = false;
+      }
 
-  //   if (isOpen) {
-  //     root.addEventListener('touchmove', preventDefault, {
-  //       passive: false,
-  //     });
-  //   }
+      if (direction === 'up' || current < HEADER_SCROLL_THRESHOLD) {
+        isVisible = true;
+      }
 
-  //   return () =>
-  //     root.removeEventListener('touchmove', preventDefault, { passive: false });
-  // }, [ isOpen ]);
-
-  ////////////////
-
-  // useEffect(() => {
-  //   if (isOpen) {
-  //     const layout = window.getComputedStyle(ref.current);
-  //     const overflow = layout.getPropertyValue('overflow');
-  //     const isHidden = overflow === 'hidden';
-  //     if (isOpen && !isHidden) {
-  //       ref.current.style.setProperty('overflow', 'hidden');
-  //     }
-  //   }
-  // }, [ isOpen ]);
+      setIsVisible(isVisible);
+    }
+    else {
+      setIsVisible(true);
+    }
+  }, [ isMobile, scroll ]);
 
   return (
     <MotionConfig isValidProp={isValidProp}>
-      <ThemeProvider theme={theme}>
-        <FontSizes />
-        <Layout ref={ref}>
-          <Header currentPath={currentPath} />
-          <Main>
-            <Component {...pageProps} />
-          </Main>
-          <Footer />
-        </Layout>
-        {/* <MobileMenu isOpen={isOpen} /> */}
-      </ThemeProvider>
+      <StyleSheetManager disableVendorPrefixes>
+        <ThemeProvider defaultTheme="dark" enableSystem={false} enableColorScheme={true}>
+          <ContextProvider theme={theme}>
+            <GlobalStyle />
+
+            <Layout ref={ref}>
+              <Header isVisible={isVisible} ref={header} />
+              <Main>
+                <Component {...pageProps} />
+              </Main>
+              <Footer />
+            </Layout>
+          </ContextProvider>
+        </ThemeProvider>
+      </StyleSheetManager>
     </MotionConfig>
   );
 }
-
-// useEffect(() => {
-//   let previousScroll;
-//   let isVisible;
-
-//   const handleScroll = () => {
-//     const currentScroll = window.scrollY;
-
-//     if (typeof previousScroll !== 'number') {
-//       previousScroll = currentScroll;
-//       return;
-//     }
-
-//     const direction = previousScroll < currentScroll ? 'down' : 'up';
-//     if (currentScroll >= HEADER_SCROLL_THRESHOLD && direction === 'down') {
-//       isVisible = false;
-//     }
-//     else if (direction === 'up') {
-//       isVisible = true;
-//     }
-
-//     setIsHeaderVisible(isVisible);
-//     previousScroll = currentScroll;
-//   };
-
-//   const throttled = throttle(handleScroll, 1500);
-
-//   const yValue = isHeaderVisible ? '0%' : '-100%';
-//   const position = `translateY(${yValue})`;
-
-//   setHeaderPosition('translateY(0%)');
-
-//   window.addEventListener('scroll', throttled);
-
-//   return () => window.removeEventListener('scroll', handleScroll);
-// }, [ isHeaderVisible, setHeaderPosition ]);
-
-// useEffect(() => {
-//   let previousScrollValue;
-
-//   function handleScroll(ev) {
-//     const currentScroll = window.scrollY;
-
-//     if (typeof previousScrollValue !== 'number') {
-//       previousScrollValue = currentScroll;
-//       return;
-//     }
-
-//     const direction = currentScroll > previousScrollValue ? 'down' : 'up';
-
-//     if (
-//       isHeaderVisible &&
-//       direction === 'down' &&
-//       currentScroll > HEADER_SCROLL_THRESHOLD
-//     ) {
-//       setIsHeaderVisible(false);
-//     }
-//     else if ((!isHeaderVisible && direction === 'up') || isOpen) {
-//       setIsHeaderVisible(true);
-//     }
-
-//     previousScrollValue = currentScroll;
-
-//     const transform = isHeaderVisible ? 'translateY(0%)' : 'translateY(-100%)';
-
-//     setHeaderPosition(transform);
-//   }
-
-//   window.addEventListener('scroll', handleScroll);
-
-//   return () => {
-//     window.removeEventListener('scroll', handleScroll);
-//   };
-// }, [ isHeaderVisible, setHeaderPosition, isOpen ]);
