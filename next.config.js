1777b50b5651d80102718c3592d98b7c742fe3a6
mattbrannon <sh4rkbot@gmail.com>
2022-04-03 18:45:41 -0500

    update next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,33 @@
-module.exports = {
-  experimental: {
-    // ssr and displayName are configured by default
-    styledComponents: true,
-    // removeConsole: {
-    //   exclude: [ 'error', 'warn' ],
-    // },
-  },
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = () => {
+  if (process.env.NODE_ENV === 'development') {
+    return {
+      compiler: {
+        styledComponents: true,
+      },
+      images: {
+        formats: [ 'image/avif', 'image/webp' ],
+      },
+      trailingSlash: true,
+      reactStrictMode: true,
+    };
+  }
+  if (process.env.NODE_ENV === 'production') {
+    console.log('starting in production environment');
+    return {
+      compiler: {
+        styledComponents: true,
+        removeConsole: { exclude: [] },
+      },
+      images: {
+        formats: [ 'image/avif', 'image/webp' ],
+      },
+      trailingSlash: true,
+      compress: true,
+      swcMinify: true,
+    };
+  }
 };
