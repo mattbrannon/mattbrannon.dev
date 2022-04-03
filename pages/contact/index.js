dc8a6cc56589308d159510c18953f58cb658a595
mattbrannon <sh4rkbot@gmail.com>
2022-04-03 18:55:33 -0500

    update contact page

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,22 +1,18 @@
 import ContactForm from '@components/ContactForm';
-import Layout from '@components/Layout';
-import PageTitle from '@components/PageTitle';
 import Head from '@components/Head';
-
-{
-  /* */
-}
+import PageTitle from '@components/PageTitle';
+import Text from '@components/Text';
 
 export default function Contact() {
   return (
     <>
       <Head title="Contact" description="If you want to get in touch, here's how.." />
-      <PageTitle>Contact Me</PageTitle>
+      <PageTitle inline>Contact Me</PageTitle>
 
-      <p>
-        Wanna get in touch? Send me a message using the form below. All messages go
-        staight to my inbox.
-      </p>
+      <Text>
+        Wanna get in touch? Send me a message using the form below. All messages go staight to my
+        inbox.
+      </Text>
       <ContactForm />
     </>
   );
