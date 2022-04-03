1aa699e3e9dc636a9f6fcca77d8903e4345ab61f
mattbrannon <sh4rkbot@gmail.com>
2022-04-03 18:57:46 -0500

    update misc home page

--- pages/misc/index.js
+++ pages/misc/index.js
@@ -1,52 +1,74 @@
+import { Link } from '@components/ExternalLink';
 import Head from '@components/Head';
-import Layout from '@components/Layout';
+import { CardHeading } from '@components/Headings';
 import PageTitle from '@components/PageTitle';
 import Spacer from '@components/Spacer';
-import { breakpoints } from '@constants/index';
-import { useMediaQuery } from '@hooks/useMediaQuery';
-import Text, { ColorText } from '@components/Text';
-import Sidenote from '@components/SideNote';
-
-import { Link } from '@components/ExternalLink';
+import Text from '@components/Text';
 import styled from 'styled-components';
-import { H3, H2 } from '@components/Headings';
+import { useState, useEffect } from 'react';
+import SideNote from '@components/SideNote';
 
-export default function Index({ posts }) {
+export default function Index() {
+  const [ isSafari, setIsSafari ] = useState(false);
+
+  useEffect(() => {
+    const isSafariBrowser = () => {
+      return navigator.userAgent.includes('Safari') && !navigator.userAgent.includes('Chrome');
+    };
+    if (isSafariBrowser()) {
+      setIsSafari(true);
+    }
+  }, []);
+
   return (
     <>
       <Head title="Oddities and revelations" description="secret shh..." />
 
       <PageTitle>Revelations & deprecations</PageTitle>
 
+      {/* <Text>
+        On this page you'll find various tools that I created and used when
+        building my site. Some of these tools are visual aids I use to help plot
+        the course of an animation. And some you can use to generate cool things
+        in your own projects.
+      </Text> */}
+
       <Text>
-        Kind of a strange title for a page huh? Yeah, I'm not really sure what to call
-        this page. Sometimes I build a tool to help me visualize something. And sometimes
-        those tools actually turn out pretty neat. That's what this page is. Basically, if
-        I were a mad scientist, this would be my secret lab. You're welcome to look around
-        and turn some knobs. Just please{' '}
-        <ColorText darkorange as="span">
-          don't feed Fred after 9:00pm.
-        </ColorText>
+        On this page you'll find various tools that I created and used when building my site.
+        Sometimes I build a tool to help me visualize something. Other times I build a tool to
+        generate some markup and wind up using quite frequently. Basically, if I were a mad
+        scientist, this would be my secret lab. You're welcome to look around and turn some knobs.
+        Just please{' '}
+        <strong>
+          <em>don't feed the cube after midnight</em>
+        </strong>
       </Text>
+      {isSafari ? (
+        <SideNote>
+          It seems like you're using Safari. The tools on this page don't do so great with Safari.
+          They still work, but you might experience some performance issues and other odd little
+          bugs. If the controls feel sluggish or things just seems a little off, I'd recommend using
+          Chrome instead.
+        </SideNote>
+      ) : null}
+
       <List>
         <Card>
-          <Link passHref href="/misc/variable-fonts">
-            <H2>Fancy Text Generator</H2>
-          </Link>
-          <Text>
-            Variable fonts + gradient backgrounds + layered text shadows = Fancy text
-          </Text>
+          <CardHeading>
+            <Link passHref href="/misc/fancy-text-generator">
+              Fancy Text Generator
+            </Link>
+          </CardHeading>
+          <Text>Variable fonts + gradient backgrounds + layered text shadows = Fancy text</Text>
         </Card>
 
         <Card>
-          <Link passHref href="/misc/experiments">
-            <H2>Experiments with a cube</H2>
-          </Link>
-          <Text>
-            Working with things in three dimensions can be challenging. This helps me to
-            visualize what's happening when working with 3d transforms. And it's also just
-            fun to move the cube around.
-          </Text>
+          <CardHeading>
+            <Link passHref href="/misc/geometry">
+              Pushing squares through circles
+            </Link>
+          </CardHeading>
+          <Text>Without geometry life is pointless...</Text>
         </Card>
       </List>
       <Spacer axis="vertical" size={32} />
@@ -58,22 +80,14 @@ const List = styled.ul`
   list-style: none;
   display: grid;
   gap: 18px;
+  padding: 0;
 `;
 
 const Card = styled.div`
   border: 1px solid black;
   border-radius: 6px;
   background: #222;
-  ${'' /* grid-column: 2; */}
   width: 100%;
   padding: 0 16px;
-
-  @media (prefers-color-scheme: light) {
-    background: hsl(223 30% 88%);
-  }
+  background: var(--basic-card-background);
 `;
-
-// Variable fonts are great. They allow you to do things you simply cannot do
-// with regular fonts. But it can be difficult to remember all the properties of
-// a specific font as well as the range of values each property accepts. This
-// playground helps me visualize and make tweaks to variable font styles.
