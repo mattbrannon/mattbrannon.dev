6096b9348d688a600615f17476b0301c90273878
mattbrannon <sh4rkbot@gmail.com>
2022-04-03 18:56:27 -0500

    update home page

--- pages/index.js
+++ pages/index.js
@@ -1,258 +1,77 @@
+import styled from 'styled-components';
+import Hero from '@components/Hero';
+import FancyTitle from '@components/FancyTitle';
 import Head from '@components/Head';
-import Layout from '@components/Layout';
-import Text, { ColorSpan } from '@components/Text';
-// import GradientText from '@components/GradientText';
-import styled, { keyframes, css } from 'styled-components';
-// import { H1 } from '@components/Headings';
-import PageTitle from '@components/PageTitle';
-import { CreatureHero } from '@components/Creature';
-import { fadeIn } from '@animations/index';
-import { motion } from 'framer-motion';
-import { InvertedButton } from '@components/Button';
 
-import { useEffect, useRef, useState } from 'react';
-import { breakpoints } from '@constants/index';
+import { useState, useEffect, useRef } from 'react';
+
 import { useMediaQuery } from '@hooks/useMediaQuery';
-import { Link } from '@components/ExternalLink';
+import { breakpoints } from '@constants/breakpoints';
+import { useHasMounted } from '@hooks/useHasMounted';
 
 export default function HomePage() {
-  const container = useRef();
-  const cubeRef = useRef();
-  const text = useRef();
-  const [ startX, setStartX ] = useState(null);
-  const [ stopX, setStopX ] = useState(null);
-  const [ containerRect, setContainerRect ] = useState({});
+  // const hasMounted = useHasMounted();
   const isMobile = useMediaQuery({ maxWidth: breakpoints.mobile });
+  const marginTop = isMobile ? 8 : 48;
+
+  const [ hasCookie, setHasCookie ] = useState(false);
+
+  const [ showTitle, setShowTitle ] = useState(false);
 
   useEffect(() => {
-    const containerExists = container && container.current;
-    const textExists = text && text.current;
-
-    if (containerExists && textExists) {
-      const containerRect = container.current.getBoundingClientRect();
-      const textRect = text.current.getBoundingClientRect();
-      const stop = containerRect.left - textRect.right;
-      const start = containerRect.width;
-      setStartX(start);
-      setStopX(stop);
-      setContainerRect(containerRect);
-      console.log({ containerRect, textRect });
+    const hasCookie = !!document.cookie.length;
+    setHasCookie(hasCookie);
+    if (hasCookie) {
+      setShowTitle(true);
     }
-  }, [ container, startX, text ]);
+  }, []);
 
   return (
-    <>
-      <Head title="Matt Brannon" description="A brief introduction" />
-      <Top>
-        <TitleWrapper>
-          <PageTitle ref={text} inline>
-            About me
-          </PageTitle>
-        </TitleWrapper>
-
-        <CreatureWrapper ref={container}>
-          <CreatureHero
-            containerRect={containerRect}
-            ref={cubeRef}
-            eyelid={42}
-            blink
-            startX={startX}
-            stopX={stopX}
-          />
-        </CreatureWrapper>
-      </Top>
-
-      {/* <Text>
-        Hey there. My name is Matt. I'm a software developer and musician. And as of
-        recently, it seems I'm also a cube. Not quite sure how that happened but, I
-        digress. <strong>Welcome to my little corner of the internet.</strong>
-      </Text> */}
-
-      <Text>
-        Hey there. I'm Fred! Matt created me to look after his website while he's off
-        doing human things. Matt's a pretty cool dude. He really enjoys making things and
-        breaking things with code. You should have seen the early versions of me. Even Dr
-        Frankestein would have been horrified. But in the end it turned out well. I mean,
-        yeah I'm a cube and he forgot to give me arms or legs but... it's fine.
-        Surprisingly enough, Matt's made quite a few other things that, for the most part,
-        work really well... unlike me. You can see some examples of those lucky programs
-        in the{' '}
-        <Link href="/apps">
-          <strong>Apps</strong>
-        </Link>{' '}
-        section of the site. I've also heard him recently tinkering around at night with
-        something. Then, the other day, a new{' '}
-        <Link href="/misc">
-          <strong>Misc</strong>
-        </Link>{' '}
-        link appeared above. So you might check that out as well. You may have also
-        noticed the{' '}
-        <Link href="/contact">
-          <strong>Contact</strong>
-        </Link>{' '}
-        link above. Just to be clear - That's for contacting Matt directly. If you do
-        reach out to him, could you also remind him that I'm still waiting for those arms
-        he promised me? I'd send him a message myself but, you know... no arms. Or, at the
-        very least, could he give me something new to read? I've read every article in his{' '}
-        <Link href="/blog">
-          {' '}
-          <strong>Blog</strong>
-        </Link>
-      </Text>
-
-      <Text>
-        For most of my life, music was the primary focus. But when you get into your 30's,
-        and you're still playing the same dirty bars you've been playing for the past ten
-        years, you start to find yourself pulled in a different direction. At least I did
-        anyway. A few years ago, I made the jump to software development, and it was
-        probably the best decision I ever made.
-      </Text>
-
-      <Text>
-        One of the best (and worst) things about software development is that you can
-        never stop learning. Except for spending three months at a coding boot camp called
-        Hack Reactor, I'm entirely self-taught. As such, I've spent years refining and
-        sharpening my ability to learn without guidance and find solutions to problems
-        autonomously.
-      </Text>
-    </>
+    <Container>
+      <Head description="Personal website for Matt Brannon" title="Matt Brannon" />
+      {!isMobile ? (
+        <TopRow style={{ '--marginTop': `${marginTop}px` }}>
+          <TitleWrapper>
+            <FancyTitle showTitle={showTitle} hasCookie={hasCookie} from="left" delay={4}>
+              Welcome to my site!
+            </FancyTitle>
+          </TitleWrapper>
+        </TopRow>
+      ) : null}
+      <Hero setShowTitle={setShowTitle} hasCookie={hasCookie} setHasCookie={setHasCookie} />
+    </Container>
   );
 }
 
-function Buttons() {
-  return (
-    <Wrapper>
-      <Button>Portfolio</Button>
-      <Button>Blog</Button>
-      <Button>Contact</Button>
-    </Wrapper>
-  );
-}
-
-const Button = styled(InvertedButton)`
-  min-width: 200px;
-  max-width: 220px;
-`;
-
-const Wrapper = styled.div`
+const Container = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(min(220px, 100%), 1fr));
-  justify-items: center;
-`;
 
-// const Button = styled.button`
-//   padding: 8px 4px;
-//   border-radius: 6px;
-//   background: var(--tealBg);
-//   font-size: var(--size40);
-//   color: white;
-//   font-weight: 800;
-//   text-shadow: 0.05em 0.05em 0.025em black;
-//   border: 0.05em solid black;
-//   box-shadow: 2px 2px 10px -4px black;
-//   margin: 8px 16px;
-//   border-radius: 14px;
-//   -webkit-touch-callout: none;
-//   -webkit-tap-highlight-color: transparent;
-//   user-select: none;
+  height: 100%;
+  --fontFamily: Recursive;
+  --fontSize: clamp(24px, 8vw, 80px);
+  --fontVariationSettings: 'wght' 974, 'slnt' -7, 'CASL' 0.42, 'CRSV' 0, 'MONO' 0;
+  --strokeWidth: 0.021875em;
+  --strokeColor: #000000;
 
-//   &:hover {
-//     background: var(--tealHover);
-//     cursor: pointer;
-//   }
-//   &:focus {
-//     background: var(--tealFocus);
-//   }
-
-//   &:active {
-//     outline: 4px solid white;
-//     outline-offset: -6px;
-//   }
-// `;
-
-// <Text></Text>
-
-// {/* <Text>
-//   Hey there. My name is Matt. I'm a software developer and a musician. I started
-//   my journey into software developemenet several years ago kinda just by chance.
-//   For most of my life, music has been my primary focus. It was only after I was an
-//   adult in my 30's that I started feeling myself pulled in another direction.
-//   Oddly enough, the whole thing started with broken computer. Maybe the fact that
-//   I was in my 30's and not a rock star had something to do with it as well.
-// </Text>
-// <Text>
-//   Several years ago, my computer was dying and I hadn't made a backup of the hard
-//   drive. Yeah, I'm THAT guy. Luckily I was able to boot into safe mode and
-//   transfer all my file to an external drive using the command prompt in safe mode.
-//   This experience made me curious about what else I could do from the command
-//   line. As it turns out, you can do quite a lot. I spent the first couple of years
-//   hacking away at bash scripts to automate various things in my daily life.
-// </Text>
-// <Text>
-//   As time went on, I eventually I found myself becoming more and more interested
-//   in Javascript. Once I figured out that I could open the developer tools and type{' '}
-//   <small>
-//     <code>document.body.style.background = 'red'</code>
-//   </small>
-//   and watch the background actually change colors, I was like, ok this is cool. So
-//   now that's what I do. I just sit around all day changing the background color of
-//   web pages. If you'd like me to change the background color of your web page, hit
-//   me up!
-// </Text> */}
-
-const MainContent = styled.div``;
-
-const Top = styled.div`
-  display: flex;
-  overflow: hidden;
-  position: relative;
   padding-bottom: 32px;
-  width: 100%;
 
   @media (max-width: ${breakpoints.mobile}px) {
-    flex-direction: column;
     padding-bottom: 0;
   }
 `;
 
-// const spin = keyframes`
-//   from {
-//     transform: rotateY(1200deg);
-//   }
-//   to {
-//     transform: rotateY(-26deg);
-//   }
-// `;
-
-// const width = keyframes`
-//   from {
-//     width: 0%;
-//   }
-//   to {
-//     width: 100%;
-//   }
-// `;
-
-const CreatureWrapper = styled(motion.div)`
-  justify-self: center;
-  align-self: flex-end;
-  transform-style: preserve-3d;
-  flex: 1;
-  margin: 32px 0;
-
-  @media (max-width: ${breakpoints.mobile}px) {
-    align-self: center;
-    margin-top: 0px;
-  }
+const TitleWrapper = styled.div`
+  top: 64px;
+  left: 0;
+  right: 0;
+  width: 100%;
+  white-space: nowrap;
+  margin: 0 auto;
+  text-align: center;
+  margin-top: var(--marginTop);
 `;
 
-// const InnerWrapper = styled.div`
-//   transform-style: preserve-3d;
-//   transform-origin: left;
-//   ${'' /* animation: ${spin} 2s ease 1s both; */}
-// `;
-
-const TitleWrapper = styled.div`
+const TopRow = styled.div`
   width: 100%;
-  flex: 1;
+  transition: margin-top, 0.2s linear;
 `;
