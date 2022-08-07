4f2129eece4a65190c0551245d2de62b2d0b3bc9
mattbrannon <sh4rkbot@gmail.com>
2022-08-06 22:53:26 -0500

    update exports

--- constants/index.js
+++ constants/index.js
@@ -1,9 +1,10 @@
 export const springDown = 'cubic-bezier(0.17, 0.88, 0.32, 1.27)';
 export const springUp = 'cubic-bezier(0.47, 1.74, 0.99, 0.99)';
-export { fonts, decovarTemplate, decovarVariables, decovarValues } from './fonts';
+export { fonts, decovarTemplate, decovarVariables, decovarValues } from './fonts.js';
 export { mobile, tablet, laptop, desktop, breakpoints } from './breakpoints';
 export { svg } from './svg';
 export { blogHeader, blogVariant } from './blog';
 export { colors } from './colors';
 export { shadows } from './shadows';
 export { gradients } from './gradients';
+export { fancyTextProps, shadowProps, gradientProps, fontProps } from './properties';
