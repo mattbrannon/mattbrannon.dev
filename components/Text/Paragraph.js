b755205126cd5bd8737750e8b792415607fd1342
mattbrannon <sh4rkbot@gmail.com>
2022-08-06 22:34:32 -0500

    group text rendering components in common folder

---
+++ components/Text/Paragraph.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import { Blockquote } from '@components/SideNote';
+
+export const P = styled.p`
+  margin: 0 0 32px 0;
+  font-size: clamp(var(--size16), 0.2vw + 1rem, var(--size20));
+  line-height: 1.7;
+
+  color: var(--color-text);
+
+  ${Blockquote} & {
+    background: palegoldenrod;
+    color: #222;
+    border-left: 8px solid gold;
+    padding: 8px 16px;
+    ${'' /* padding-left: 16px; */}
+    border-radius: 0 6px 6px 0;
+    line-height: 1.5;
+    font-size: var(--size18);
+    font-family: 'OpenSans';
+    font-variation-settings: 'wdth' 80, 'wght' 600;
+    width: 90%;
+  }
+`;
