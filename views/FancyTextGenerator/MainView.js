513ed72240201856f70dba3dddf74b2f3ef899c3
Matt Brannon <mattbrannon.dev@gmail.com>
2022-08-26 10:29:06 -0500

    tweak styles

--- views/FancyTextGenerator/MainView.js
+++ views/FancyTextGenerator/MainView.js
@@ -1,7 +1,7 @@
 import { useEffect, useState, useRef } from 'react';
 import { AnimatePresence, m as motion, useAnimation } from 'framer-motion';
 import { textGeneratorVariant } from '@animations/variants';
-import { FancyText, FancyInput, FancyGradient, ContentEditable, Span } from './styles';
+import { FancyText, FancyInput, FancyGradient, ContentEditable, Span, ViewWrapper } from './styles';
 
 export const FancyAnimatedGradient = ({ state, children, ...props }) => {
   const [duration, setDuration] = useState(2);
@@ -16,45 +16,52 @@ export const FancyAnimatedGradient = ({ state, children, ...props }) => {
   };
 
   const handleAnimationComplete = (name) => {
-    if (state.reset && name === 'show') {
+    if (state.reset) {
       props.dispatch({ type: 'reset', value: false });
+      setDuration(5);
     }
     console.log(name, 'complete');
   };
 
   return (
-    <AnimatePresence exitBeforeEnter>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        key={state.styles.fontFamily}
-        transition={{ duration: 1 }}
-        state={state}
-        style={{
-          '--fontFamily': state.styles.fontFamily,
-          '--fontSize': state.styles.fontSize + 'px',
-          '--fontSettings': state.styles.fontVariationSettings,
-          '--gradient': state.styles.gradient,
-          '--shadow': state.styles.shadow,
-          '--strokeWidth': state.styles.strokeWidth + 'em',
-          '--strokeColor': state.styles.strokeColor,
-          '--letterSpacing': state.styles.letterSpacing + 'em',
-          padding: 48,
-        }}
-      >
-        <FancyGradient
-          initial="hidden"
-          animate="show"
-          exit="close"
-          variants={textGeneratorVariant}
-          onAnimationStart={handleAnimationStart}
-          onAnimationComplete={handleAnimationComplete}
-          custom={{ duration, state }}
+    <ViewWrapper>
+      <AnimatePresence exitBeforeEnter>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          key={state.styles.fontFamily}
+          transition={{ duration: 0.5, ease: 'easeInOut' }}
           state={state}
-          {...props}
-        />
-      </motion.div>
-    </AnimatePresence>
+          style={{
+            '--fontFamily': state.styles.fontFamily,
+            '--fontSize': state.styles.fontSize + 'px',
+            '--fontSettings': state.styles.fontVariationSettings,
+            '--gradient': state.styles.gradient,
+            '--shadow': state.styles.shadow,
+            '--strokeWidth': state.styles.strokeWidth + 'em',
+            '--strokeColor': state.styles.strokeColor,
+            '--letterSpacing': state.styles.letterSpacing + 'em',
+            padding: 48,
+            height: '100%',
+            overflow: 'hidden',
+            textAlign: 'center',
+          }}
+        >
+          <FancyGradient
+            initial="hidden"
+            animate="show"
+            exit="close"
+            variants={textGeneratorVariant}
+            onAnimationStart={handleAnimationStart}
+            onAnimationComplete={handleAnimationComplete}
+            custom={{ duration, state }}
+            state={state}
+            id={props.mainContent}
+            {...props}
+          />
+        </motion.div>
+      </AnimatePresence>
+    </ViewWrapper>
   );
 };
