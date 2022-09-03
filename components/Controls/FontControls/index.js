f8c21baf86ab984dc993baa50ae382c08a1b6801
Matt Brannon <mattbrannon.dev@gmail.com>
2022-09-03 09:07:37 -0500

    remove unused controls

--- components/Controls/FontControls/index.js
+++ components/Controls/FontControls/index.js
@@ -5,7 +5,7 @@ import { FontProperties } from './FontProperties';
 import { Layout, Group, SectionHeading } from './Layout';
 import { Buttons } from './Buttons';
 // import Checkbox from '@components/Checkbox';
-import Select, { Option } from '@components/Select';
+// import Select, { Option } from '@components/Select';
 
 export const FontControls = forwardRef(Controls);
 
@@ -28,22 +28,11 @@ function Controls({ ...props }, ref) {
 
       <Group>
         <SectionHeading>Gradient</SectionHeading>
-        <Select
-          value={applyToWords ? 'apply to words' : 'apply to phrase'}
-          onChange={(e) =>
-            props.dispatch({ type: 'APPLY_TO_WORDS', value: e.target.value === 'apply to words' })
-          }
-        >
-          {['apply to words', 'apply to phrase'].map((choice, i) => (
-            <option key={i}>{choice}</option>
-          ))}
-        </Select>
         <Color {...props} name="gradient color start" value={gradient.gradientColorStart} />
         <Color {...props} name="gradient color end" value={gradient.gradientColorEnd} />
         <Range {...props} name="gradient angle" value={gradient.gradientAngle} max={360} />
         <Range {...props} name="gradient midpoint" value={gradient.gradientMidpoint} />
         <Range {...props} name="gradient blend" value={gradient.gradientBlend} />
-        {/* <Checkbox {...props} name="apply to words" value={props.state.applyToWords} /> */}
       </Group>
 
       <Group>
@@ -84,7 +73,7 @@ function Controls({ ...props }, ref) {
           name="letter spacing"
           value={styles.letterSpacing}
           step={0.001}
-          min={0}
+          min={-0.25}
           max={0.25}
         />
         <Range {...props} name="font size" value={styles.fontSize} step={1} min={16} max={160} />
