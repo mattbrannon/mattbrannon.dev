2dd370468b0fc44baca2a92d0218dc0cbf2f8ca6
Matt Brannon <mattbrannon.dev@gmail.com>
2022-09-03 10:03:45 -0500

    refactor

--- views/FancyTextGenerator/MainView.js
+++ views/FancyTextGenerator/MainView.js
@@ -1,14 +1,14 @@
-import { useEffect, useState, useRef } from 'react';
+import { useState } from 'react';
 import { AnimatePresence, m as motion, useAnimation } from 'framer-motion';
 import { textGeneratorVariant } from '@animations/variants';
-import { FancyText, FancyInput, FancyGradient, ContentEditable, Span, ViewWrapper } from './styles';
+import styled from 'styled-components';
 
-export const FancyAnimatedGradient = ({ state, children, ...props }) => {
+export const MainView = ({ state, dispatch, children, ...props }) => {
   const [duration, setDuration] = useState(2);
 
   const handleAnimationStart = (name) => {
     if (name === 'close') {
-      setDuration(2);
+      setDuration(1);
     }
     else {
       setDuration(0);
@@ -17,51 +17,129 @@ export const FancyAnimatedGradient = ({ state, children, ...props }) => {
 
   const handleAnimationComplete = (name) => {
     if (state.reset) {
-      props.dispatch({ type: 'reset', value: false });
+      dispatch({ type: 'reset', value: false });
       setDuration(5);
     }
-    console.log(name, 'complete');
+  };
+
+  const handleChange = (e) => {
+    const value = e.target.value.replace(/\s+/g, ' ').replace(/'/g, '\u{2019}') || 'Your text here';
+
+    dispatch({
+      type: 'TEXT_CONTENT',
+      value: e.target.value.replace(/\s+/g, ' ').replace(/'/g, '\u{2019}'),
+    });
   };
 
   return (
-    <ViewWrapper>
+    <div style={{ padding: '64px' }}>
       <AnimatePresence exitBeforeEnter>
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          key={state.styles.fontFamily}
-          transition={{ duration: 0.5, ease: 'easeInOut' }}
+        <Container
+          id="main-content"
           state={state}
-          style={{
-            '--fontFamily': state.styles.fontFamily,
-            '--fontSize': state.styles.fontSize + 'px',
-            '--fontSettings': state.styles.fontVariationSettings,
-            '--gradient': state.styles.gradient,
-            '--shadow': state.styles.shadow,
-            '--strokeWidth': state.styles.strokeWidth + 'em',
-            '--strokeColor': state.styles.strokeColor,
-            '--letterSpacing': state.styles.letterSpacing + 'em',
-            padding: 48,
-            height: '100%',
-            overflow: 'hidden',
-            textAlign: 'center',
-          }}
+          initial="hidden"
+          animate="show"
+          exit="close"
+          variants={textGeneratorVariant}
+          onAnimationStart={handleAnimationStart}
+          onAnimationComplete={handleAnimationComplete}
+          key={state.styles.fontFamily}
+          custom={{ duration, state }}
         >
-          <FancyGradient
-            initial="hidden"
-            animate="show"
-            exit="close"
-            variants={textGeneratorVariant}
-            onAnimationStart={handleAnimationStart}
-            onAnimationComplete={handleAnimationComplete}
-            custom={{ duration, state }}
-            state={state}
-            id={props.mainContent}
-            {...props}
-          />
-        </motion.div>
+          <GradientSpan state={state}>{state.textContent}</GradientSpan>
+
+          <Wrapper state={state}>
+            <TextAreaBox placeholder="edit me" onInput={handleChange} value={state.textContent} />
+          </Wrapper>
+        </Container>
       </AnimatePresence>
-    </ViewWrapper>
+    </div>
   );
 };
+
+const Container = styled(motion.div).attrs(({ state }) => {
+  return {
+    style: {
+      '--fontFamily': state.styles.fontFamily,
+      '--fontSize': state.styles.fontSize + 'px',
+      '--fontVariationSettings': state.styles.fontVariationSettings,
+      '--gradient': state.styles.gradient,
+      '--shadow': state.styles.shadow,
+      '--strokeWidth': state.styles.strokeWidth + 'em',
+      '--strokeColor': state.styles.strokeColor,
+      '--letterSpacing': state.styles.letterSpacing + 'em',
+    },
+  };
+})`
+  font-family: var(--fontFamily);
+  font-size: var(--fontSize);
+  font-variation-settings: var(--fontVariationSettings);
+  letter-spacing: var(--letterSpacing);
+
+  overflow-wrap: break-word;
+  word-wrap: break-word;
+  -ms-word-break: break-all;
+  word-break: break-word;
+  position: relative;
+`;
+
+const GradientSpan = styled.span`
+  font: inherit;
+  background: var(--gradient);
+  background-clip: text;
+  color: transparent;
+  box-decoration-break: clone;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  -webkit-box-decoration-break: clone;
+  margin: 0 -32px;
+  padding-left: 32px;
+  padding-right: 32px;
+`;
+
+const Wrapper = styled(motion.div)`
+  display: grid;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  &::after {
+    content: '${(p) => p.state.textContent} \u{00a0}';
+    white-space: pre-wrap;
+    border: none;
+    font: inherit;
+    color: transparent;
+    text-shadow: var(--shadow);
+    z-index: -1;
+    -webkit-text-stroke: var(--strokeWidth) var(--strokeColor);
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+  }
+`;
+
+const TextAreaBox = styled.textarea.attrs({ rows: 1, spellCheck: false })`
+  resize: none;
+  overflow: hidden;
+  border: none;
+  font: inherit;
+  background: none;
+  color: transparent;
+  -webkit-text-fill-color: transparent;
+  caret-color: var(--fancyCaretColor);
+  letter-spacing: var(--letterSpacing);
+
+  margin: -32px;
+  padding: 32px;
+
+  &::placeholder {
+    -webkit-text-fill-color: hsl(0, 0%, 97%, 0.7);
+    /* -webkit-text-stroke: 0.0125em black; */
+    text-shadow: -0.0125em -0.0125em 0em #777, -0.025em -0.025em 0em #555,
+      -0.0375em -0.0375em 0em #333, -0.05em -0.05em 0em #111;
+  }
+`;
--- views/FancyTextGenerator/styles.js
+++ views/FancyTextGenerator/styles.js
@@ -1,18 +1,20 @@
 import styled from 'styled-components';
-import { m as motion, useMotionValue } from 'framer-motion';
-import { Children, useState, useEffect, useRef } from 'react';
+import { m as motion } from 'framer-motion';
 import FancyTitle from '@components/FancyTitle';
+import { breakpoints } from '@constants/breakpoints';
 
-export const Main = styled.div`
+export const Article = styled.article`
   position: absolute;
   top: var(--header-height);
   left: var(--controlWidth);
   right: 0;
   bottom: var(--footer-height);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow-x: hidden;
+  overflow: auto;
+
+  @media (max-width: ${breakpoints.mobile}) {
+    --controlWidth: 0;
+    bottom: 50%;
+  }
 `;
 
 export const NoScript = styled.noscript`
@@ -70,89 +72,23 @@ export const Text = styled.p`
 //// CodeView
 
 export const ViewWrapper = styled(motion.div)`
-  position: absolute;
-  width: 100%;
+  /* position: absolute; */
+  /* width: 100%; */
   height: 100%;
   overflow: auto;
+  /* overflow: auto;
   left: 0;
   right: 0;
   top: 0;
+  bottom: 0; */
+
+  @media (max-width: ${breakpoints.mobile}) {
+    left: 0;
+    right: 0;
+  }
 `;
 
 /// MainView
-
-// export const ContentEditable = ({ children, ...props }) => {
-//   const [text, setText] = useState('');
-//   console.log(props);
-//   return (
-//     <P
-//       style={props.styles}
-//       onInput={(e) => setText(e.target.textContent)}
-//       text={text}
-//       spellCheck={false}
-//       contentEditable={true}
-//     >
-//       {children}
-//     </P>
-//   );
-// };
-
-// export const P = styled.p`
-//   height: 100%;
-//   width: calc(100vw - var(--controlWidth));
-//   margin: 0;
-//   padding: 32px;
-//   outline: none;
-//   font-family: var(--fontFamily);
-//   font-variation-settings: var(--fontSettings);
-//   font-size: var(--fontSize);
-//   letter-spacing: var(--letterSpacing);
-
-//   background-image: var(--gradient);
-//   text-shadow: var(--shadow);
-
-//   &:before {
-//     ${
-//   '' /* background-clip: text;
-//     -webkit-background-clip: text;
-//     color: transparent;
-//     -webkit-text-fill-color: transparent;
-//     content: '${({ children }) => children}';
-//     position: absolute;
-//     z-index: -1;
-
-//     text-shadow: var(--shadow);
-//     -webkit-text-stroke: var(--strokeWidth) var(--strokeColor); */
-// }
-//   }
-
-//   &:after {
-//     text-shadow: none;
-//     color: red;
-//     content: '${({ children }) => children}';
-//     position: absolute;
-//     top: 0;
-//     padding: 32px;
-//   }
-// `;
-
-// export const FancyText = ({ children, ...props }) => {
-//   return (
-//     <>
-//       <div style={{ height: '120px' }}></div>
-//       <div style={{ top: '48px', padding: '120px 48px' }}>
-//         {children.split(' ').map((word, i, collection) => {
-//           const space = i < collection.length - 1;
-//           return (
-//             <Word space={space} key={i}>
-//               {word}
-//             </Word>
-//           );
-//         })}
-//       </div>
-//     </>
-//   );
-// };
 
 export const Span = styled(motion.span)`
   font-family: var(--fontFamily);
@@ -161,31 +97,21 @@ export const Span = styled(motion.span)`
   position: relative;
   display: inline-block;
 
-  /* width: max-content; */
-
   -webkit-background-clip: text;
   background-clip: text;
-  -webkit-text-fill-color: transparent;
-  color: transparent;
+  /* -webkit-text-fill-color: transparent; */
+  /* color: transparent; */
   padding-bottom: 0.2em;
   padding-left: 0.44em;
   margin-left: -0.44em;
   margin-bottom: -0.2em;
-  /* padding-right: 0.1em; */
-
-  /* padding: 0.125em 0.25em 0.125em 0.25em; */
-
   background-image: var(--gradient);
   letter-spacing: var(--letterSpacing);
 
   &:after {
     content: '${(p) => p.children}';
-    /* padding: 0.125em 0.25em 0.125em 0.25em; */
     padding-bottom: 0.2em;
     padding-left: 0.44em;
-
-    /* padding-right: 0.1em; */
-
     position: absolute;
     top: 0;
     left: 0;
@@ -195,42 +121,28 @@ export const Span = styled(motion.span)`
   }
 `;
 
-export const WordSpan = styled(Span)`
-  padding-top: 12px;
-  padding-bottom: 12px;
-  padding-left: 0px;
-  padding-right: 0px;
+// export const WordSpan = styled(Span)`
+//   padding-top: 12px;
+//   padding-bottom: 12px;
+//   padding-left: 0px;
+//   padding-right: 0px;
 
-  &:after {
-    padding-top: 12px;
-    padding-bottom: 12px;
-    padding-left: 0px;
-    padding-right: 0px;
-  }
-
-  /* &:nth-of-type(1) {
-    padding-left: 42px;
-  }
-  &:nth-of-type(1):after {
-    padding-left: 42px;
-  } */
-`;
-
-// const Heading = styled(H1)`
-//   font-size: clamp(48px, 4vw, 60px);
-//   margin-bottom: 0;
-//   margin-top: 0;
-//   letter-spacing: var(--letterSpacing);
+//   &:after {
+//     padding-top: 12px;
+//     padding-bottom: 12px;
+//     padding-left: 0px;
+//     padding-right: 0px;
+//   }
 // `;
 
-const Word = ({ children, ...props }) => {
-  if (props.space) {
-    return <Span {...props}>{children}&nbsp;</Span>;
-  }
-  else {
-    return <Span {...props}>{children}</Span>;
-  }
-};
+// const Word = ({ children, ...props }) => {
+//   if (props.space) {
+//     return <Span {...props}>{children}&nbsp;</Span>;
+//   }
+//   else {
+//     return <Span {...props}>{children}</Span>;
+//   }
+// };
 
 export const FancyGradient = ({ ...props }) => {
   const onChange = (e) => {
@@ -240,11 +152,7 @@ export const FancyGradient = ({ ...props }) => {
 
   const Gradient = props.state.applyToWords ? WordGradient : PhraseGradient;
 
-  return (
-    <div>
-      <Gradient onChange={onChange} {...props} />
-    </div>
-  );
+  return <Gradient onChange={onChange} {...props} />;
 };
 
 const PhraseGradient = ({ ...props }) => {
@@ -276,87 +184,6 @@ const WordGradient = ({ ...props }) => {
   );
 };
 
-// export const WordSpan = styled(motion.span)`
-//   font-family: var(--fontFamily);
-//   font-variation-settings: var(--fontSettings);
-//   font-size: var(--fontSize);
-//   position: relative;
-//   display: inline-block;
-//   /* width: max-content; */
-
-//   -webkit-background-clip: text;
-//   background-clip: text;
-//   -webkit-text-fill-color: transparent;
-//   color: transparent;
-
-//   background-image: var(--gradient);
-//   letter-spacing: var(--letterSpacing);
-
-//   padding-bottom: 4px;
-
-//   &:after {
-//     padding-bottom: 4px;
-
-//     content: '${(p) => p.children}';
-
-//     position: absolute;
-//     top: 0;
-//     left: 0;
-//     z-index: -1;
-//     text-shadow: var(--shadow);
-//     -webkit-text-stroke: var(--strokeWidth) var(--strokeColor);
-//   }
-// `;
-
-const Editable = styled.div.attrs(() => {
-  return { contentEditable: true };
-})`
-  font-size: var(--fontSize);
-  padding: 16px;
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: transparent;
-  letter-spacing: var(--letterSpacing);
-  text-align: center;
-  /* font-weight: 900; */
-  font-family: var(--fontFamily);
-  font-variation-settings: var(--fontSettings);
-  caret-color: red;
-  color: red;
-  /* color: transparent; */
-  /* outline: none; */
-  border: none;
-`;
-
-export const FancyHeading = styled.span`
-  background-clip: text;
-  -webkit-background-clip: text;
-  color: transparent;
-  -webkit-text-fill-color: transparent;
-  font-family: var(--fontFamily);
-  font-variation-settings: var(--fontSettings);
-  font-size: var(--fontSize);
-
-  background-image: var(--gradient);
-  padding: 24px;
-
-  ${'' /* white-space: nowrap; */}
-
-  letter-spacing: var(--letterSpacing);
-
-  &:before {
-    content: '${({ children }) => children}';
-    position: absolute;
-    z-index: -1;
-
-    text-shadow: var(--shadow);
-    -webkit-text-stroke: var(--strokeWidth) var(--strokeColor);
-  }
-`;
-
 export const FancyInput = styled.input.attrs(() => {
   return {
     spellCheck: false,
@@ -375,144 +202,59 @@ export const FancyInput = styled.input.attrs(() => {
   text-align: center;
   font-family: var(--fontFamily);
   font-variation-settings: var(--fontSettings);
-  caret-color: transparent;
+  /* caret-color: transparent; */
+  caret-color: white;
   color: transparent;
   /* outline: none; */
   border: none;
   z-index: 1000;
 `;
 
-// import styled from 'styled-components';
-// import { m as motion, useMotionValue } from 'framer-motion';
+const FancyGradientShadow = ({ ...props }) => {
+  const words = props.state.textContent.split(' ');
 
-// export const Main = styled.div`
-//   position: absolute;
-//   top: var(--header-height);
-//   left: var(--controlWidth);
-//   right: 0;
-//   bottom: var(--footer-height);
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   overflow-x: hidden;
-// `;
-
-// // HelpView
-// export const Container = styled.div`
-//   background: var(--help-background);
-//   color: var(--color-text);
-// `;
-
-// export const Wrapper = styled.ul`
-//   margin-left: 0;
-//   margin-top: 0;
-// `;
-
-// export const Heading = styled.h3`
-//   margin-top: 0;
-//   padding: 32px 0 0 0;
-//   color: var(--h3);
-// `;
-
-// export const SubHeading = styled.h4`
-//   color: var(--fancy-text-color);
-//   margin: 0 48px 16px 32px;
-// `;
-
-// export const Row = styled.li`
-//   margin: 0 48px 16px 32px;
-//   display: grid;
-//   grid-template-columns: 120px auto;
-// `;
-
-// export const Section = styled.section`
-//   padding: 0 0 32px 0;
-//   background: inherit;
-// `;
-
-// export const Text = styled.p`
-//   margin: 0 48px 16px 32px;
-// `;
-
-// //// CodeView
-
-// export const ViewWrapper = styled(motion.div)`
-//   position: absolute;
-//   width: 100%;
-//   height: 100%;
-//   overflow: auto;
-//   left: 0;
-//   right: 0;
-//   top: 0;
-// `;
-
-// /// MainView
-
-export const FancyText = ({ children, ...props }) => {
-  return <FancyHeading {...props}>{children}</FancyHeading>;
+  return (
+    <>
+      <ShadowLayer />
+    </>
+  );
 };
 
-// export const FancyText = styled.h1`
-//   background-clip: text;
-//   -webkit-background-clip: text;
-//   color: transparent;
-//   -webkit-text-fill-color: transparent;
-//   font-family: var(--fontFamily);
-//   font-variation-settings: var(--fontSettings);
-//   font-size: var(--fontSize);
+const TextArea = styled(motion.textarea).attrs({ maxLength: 200, spellCheck: false })`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  resize: none;
+  background: transparent;
+  font-size: var(--fontSize);
+  font-family: var(--fontFamily);
+  font-variation-settings: var(--fontVariationSettings);
+  letter-spacing: var(--letterSpacing);
+  color: transparent;
+  caret-color: var(--fancyCaretColor);
+  padding: 3rem;
+  overflow: hidden;
+`;
 
-//   background-image: var(--gradient);
-//   padding: 24px;
+const ShadowLayer = styled(TextArea)`
+  text-shadow: var(--shadow);
+  color: transparent;
+  font-size: var(--fontSize);
+  background: transparent;
+  -webkit-text-stroke: var(--strokeWidth) var(--strokeColor);
+  overflow: hidden;
+`;
 
-//   white-space: nowrap;
+const GradientLayer = styled(TextArea)`
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
 
-//   letter-spacing: var(--letterSpacing);
-
-//   will-change: font-variation-settings, background-image;
-
-//   &:before {
-//     content: '${({ children }) => children}';
-//     position: absolute;
-//     z-index: -1;
-
-//     text-shadow: var(--shadow);
-
-//     will-change: text-shadow;
-
-//     -webkit-text-stroke: var(--strokeWidth) var(--strokeColor);
-//   }
-// `;
-
-// export const FancyGradient = ({ ...props }) => {
-//   const onChange = (e) => {
-//     const text = e.target.value.replace("'", '`');
-//     props.dispatch({ type: 'TEXT_CONTENT', value: text });
-//   };
-
-//   const Gradient = props.state.applyToWords ? WordGradient : PhraseGradient;
-
-//   return (
-//     <div style={{ maxWidth: '100%', margin: '0 auto' }}>
-//       <Gradient onChange={onChange} {...props} />
-//     </div>
-//   );
-
-//   // const words = props.state.textContent.split(' ');
-//   // return (
-//   //   <div style={{ maxWidth: '100%', margin: '0 auto' }}>
-//   //     <FancyInput value={props.state.textContent} onChange={onChange} />
-//   //     {words.map((word, i) => (
-//   //       <WordSpan {...props} key={i}>
-//   //         {word}&nbsp;
-//   //       </WordSpan>
-//   //     ))}
-//   //   </div>
-//   // );
-
-//   // return (
-//   //   <div style={{ maxWidth: '100%', margin: '0 auto' }}>
-//   //     <FancyInput value={props.state.textContent} onChange={handleChange} />
-//   //     <Span {...props}>{props.state.textContent}</Span>
-//   //   </div>
-//   // );
-// };
+  background-image: var(--gradient);
+  font-size: var(--fontSize);
+  overflow: hidden;
+`;
