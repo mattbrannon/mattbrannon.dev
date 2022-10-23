1dbfa5183ecf478aab796920b03c53e50dcf8686
Matt Brannon <mattbrannon.dev@gmail.com>
2022-10-23 06:53:39 -0500

    Move mini game to separate component

--- pages/apps/monty-hall/index.js
+++ pages/apps/monty-hall/index.js
@@ -1,22 +1,21 @@
 import { AppTitle } from '@components/AppTitle';
-import { button } from '@components/Button';
 import Head from '@components/Head';
 import { H2Link, H3Link } from '@components/Headings';
 import PageButtons from '@components/PageButtons';
-import { SideNote } from '@components/SideNote';
 import { text } from '@components/Text';
-// import Image from 'next/future/image';
 import { useState, useContext, memo } from 'react';
 import styled, { ThemeContext } from 'styled-components';
 import { ExternalLink } from '@components/Links';
 
 import image1Avif from '/public/images/monty-hall/monty-hall1.avif';
+import image1WebP from '/public/images/monty-hall/monty-hall1.webp';
+import image1Png from '/public/images/monty-hall/monty-hall1.png';
+import { Picture } from '@components/Picture';
 
-import Image from 'next/image';
 import { AnimatePresence, m as motion } from 'framer-motion';
 import { VideoPlayer } from '@components/VideoPlayer';
 import { spacer } from '@components/Spacer';
-import { MaxWidthWrapper } from '@components/MaxWidthWrapper';
+import { MiniGame } from '@components/MiniGame';
 
 const links = {
   github: 'https://github.com/mattbrannon/monty-hall-experiment',
@@ -43,18 +42,20 @@ export default memo(function MontyHallPage() {
 
       <section>
         <Heading>What is the Monty Hall Problem?</Heading>
+
         <text.paragraph>
           The Monty Hall Problem is a logic puzzle originally posed by Steve Selvin in a letter written to the American
           Statistician in 1975. It was made famous when a reader of Parade Magazine's <em>Ask Marilyn</em> column posed
           the same question to Marilyn Vos Savant
         </text.paragraph>
-        <MiniGame setGameState={setGameState} isCorrect={isCorrect} setIsCorrect={setIsCorrect} />
+
+        <MiniGame setGameState={setGameState} isCorrect={isCorrect} />
       </section>
 
       {isCorrect !== null || context.hasPlayedGame ? (
         <AnimatePresence>
           <motion.section key="1" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
-            <Heading>Explanation</Heading>
+            <k>Explanation</k>
             <text.paragraph>
               If you've never encountered this problem before, the idea that switching doors would increase your chances
               of winning very likely seems wrong. How could switching doors increase your chances? Let's look at a
@@ -64,16 +65,16 @@ export default memo(function MontyHallPage() {
             <Table />
 
             <text.paragraph>Let's say we pick door #1.</text.paragraph>
-            <FirstChoice />
+            <Table1 />
 
             <text.paragraph>
               Our host, Monty Hall, then opens door #2, revealing a goat and asks if we want to switch doors.
             </text.paragraph>
 
-            <GreyedOut />
+            <Table2 />
 
             <text.paragraph>Keep in mind, door #2 is now out of play.</text.paragraph>
-            <SecondChoice />
+            <Table3 />
 
             <text.paragraph>
               Here comes the tricky part. Our original odds haven't changed. We had a 1 in 3 chance of selecting the
@@ -81,7 +82,7 @@ export default memo(function MontyHallPage() {
               fact cannot change those odds.
             </text.paragraph>
 
-            <OddsColors />
+            <Table4 />
 
             <text.paragraph>
               Similarly the odds of the winning door being one of the other two hasn't changed either. When Monty
@@ -90,9 +91,9 @@ export default memo(function MontyHallPage() {
               3.
             </text.paragraph>
 
-            <OutOfPlay />
+            <Table5 />
 
-            <SubHeading>Alternative Reasoning</SubHeading>
+            <SubHeading>Alternative Approach</SubHeading>
             <text.paragraph>
               An alternative way to think about it is to just go through each possibility and look at the results of
               switching versus not switching. If you do that, you'll start to notice a pattern. If we always switch
@@ -101,10 +102,8 @@ export default memo(function MontyHallPage() {
               being the last remaining door after Monty reveals a goat.
             </text.paragraph>
 
-            {/* <MaxWidthWrapper size={'80ch'}> */}
-            <Breakdown />
+            <Table6 />
             <small>Every possible round of play</small>
-            {/* </MaxWidthWrapper> */}
 
             <spacer.block size={64} />
 
@@ -139,9 +138,8 @@ export default memo(function MontyHallPage() {
               others are written in a more modern style with hooks.
             </text.paragraph>
 
-            <FlexContainer>
-              <Image src={image1Avif} alt="image1" />
-            </FlexContainer>
+            <Picture sources={[image1Avif, image1WebP, image1Png]} alt="image1" />
+
             <text.paragraph>
               This is one of those projects that I always come back to at some point in time. It's a cool game with semi
               decent visual appeal. But it could be so much better. At the time I wrote it, I thought the video playing
@@ -156,11 +154,6 @@ export default memo(function MontyHallPage() {
               <ExternalLink href="https://monty-hall-experiment.surge.sh/">The Monty Hall Experiment</ExternalLink> if
               you'd like to play it yourself.
             </text.paragraph>
-
-            {/* <div style={{ height: '16px' }}></div>
-            <FlexContainer>
-              <Image src={image3Avif} alt="image3" />
-            </FlexContainer> */}
           </motion.section>
         </AnimatePresence>
       ) : null}
@@ -169,11 +162,6 @@ export default memo(function MontyHallPage() {
     </article>
   );
 });
-
-const FlexContainer = styled.div`
-  display: flex;
-  gap: 4px;
-`;
 
 const Heading = styled(H2Link)`
   font-size: var(--size28);
@@ -185,161 +173,28 @@ const SubHeading = styled(H3Link)`
   color: var(--h4);
 `;
 
-const Results = ({ isCorrect }) => {
-  const display = isCorrect !== null ? (isCorrect ? "That's correct!" : "Sorry, that's incorrect") : null;
-
-  const color = isCorrect ? 'var(--correct-answer)' : 'var(--wrong-answer)';
-
-  return (
-    <ResultContainer>
-      <Result style={{ '--color': color }}>{display}</Result>
-      <div>
-        <Small>You have about a 67% chance of winning if you switch doors</Small>
-      </div>
-    </ResultContainer>
-  );
-};
-
-const Buttons = ({ isCorrect, setGameState }) => {
-  return (
-    <AnimatePresence exitBeforeEnter>
-      {isCorrect === null ? (
-        <ButtonGroup
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.2 }}
-          key="buttons"
-        >
-          <ButtonWrapper>
-            <button.teal onClick={() => setGameState(true)}>Yes</button.teal>
-          </ButtonWrapper>
-          <ButtonWrapper>
-            <button.teal onClick={() => setGameState(false)}>No</button.teal>
-          </ButtonWrapper>
-        </ButtonGroup>
-      ) : (
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.2 }}
-          key="results"
-        >
-          <Results isCorrect={isCorrect} />
-        </motion.div>
-      )}
-    </AnimatePresence>
-  );
-
-  // if (isCorrect === null) {
-  //   return (
-  //     <ButtonGroup>
-  //       <ButtonWrapper>
-  //         <button.teal onClick={() => setGameState(true)}>Yes</button.teal>
-  //       </ButtonWrapper>
-  //       <ButtonWrapper>
-  //         <button.teal onClick={() => setGameState(false)}>No</button.teal>
-  //       </ButtonWrapper>
-  //     </ButtonGroup>
-  //   );
-  // }
-  // else {
-  //   return <Results isCorrect={isCorrect} />;
-  // }
-};
-
-const Question = ({ ...props }) => {
-  return (
-    <>
-      <SideNote>
-        Suppose you're on a game show, and you're given the choice of three doors: Behind one door is a car; behind the
-        others, goats. You pick a door, say No. 1, and the host, who knows what's behind the doors, opens another door,
-        say No. 3, which has a goat. He then says to you, "Do you want to pick door No. 2?"
-        <br />
-        <br />
-        <b> Is it to your advantage to switch your choice?</b>
-      </SideNote>
-
-      <Buttons {...props} />
-    </>
-  );
-};
-
-function MiniGame({ ...props }) {
-  return <Question {...props} />;
-}
-
-const ResultContainer = styled.div`
-  width: 100%;
-  display: grid;
-  place-content: center;
-  font-variation-settings: 'wdth' 120, 'wght' 700;
-  /* margin: 96px 0; */
-  min-height: 140px;
-`;
-
-const Result = styled.div`
-  font-weight: 700;
-  text-align: center;
-  color: var(--color);
-`;
-
-const Small = styled.small`
-  display: block;
-  text-align: center;
-`;
-
-// const FancyButton = styled(button.teal)`
-//   flex: 1;
-//   width: 100%;
-// `;
-
-const ButtonGroup = styled(motion.div)`
-  display: flex;
-  gap: clamp(16px, 5vw, 32px);
-  align-items: center;
-  flex-wrap: wrap;
-  justify-content: space-evenly;
-
-  margin: 32px 0;
-  padding: 32px 0;
-  margin: auto;
-  width: 100%;
-  min-height: 140px;
-`;
-
-const ButtonWrapper = styled.div`
-  max-width: 160px;
-  width: 100%;
-  color: white;
-`;
-
-const ImageContainer = styled.div`
-  display: block;
-  width: 100%;
-`;
-
-const ResponsiveImage = ({ ...props }) => {
-  return (
-    <ImageContainer>
-      <Image {...props} layout="responsive" alt={props.alt} />
-    </ImageContainer>
-  );
-};
-
 const Table = () => {
   return (
     <table style={{ width: '100%' }}>
-      <TableHead />
+      <thead>
+        <tr>
+          <th>Door 1</th>
+          <th>Door 2</th>
+          <th>Door 3</th>
+        </tr>
+      </thead>
       <tbody>
-        <Option1 />
+        <tr style={{ textAlign: 'center' }}>
+          <td>Goat</td>
+          <td>Goat</td>
+          <td>Car</td>
+        </tr>
       </tbody>
     </table>
   );
 };
 
-const FirstChoice = () => {
+const Table1 = () => {
   return (
     <table style={{ width: '100%' }}>
       <thead>
@@ -360,7 +215,7 @@ const FirstChoice = () => {
   );
 };
 
-const GreyedOut = () => {
+const Table2 = () => {
   return (
     <table style={{ width: '100%' }}>
       <thead>
@@ -381,7 +236,7 @@ const GreyedOut = () => {
   );
 };
 
-const SecondChoice = () => {
+const Table3 = () => {
   return (
     <table style={{ width: '100%' }}>
       <thead>
@@ -402,28 +257,7 @@ const SecondChoice = () => {
   );
 };
 
-const OutOfPlay = () => {
-  return (
-    <table style={{ width: '100%' }}>
-      <thead>
-        <tr style={{ display: 'grid', gridTemplateColumns: '1fr 2fr' }}>
-          <th style={{ background: 'lightblue', color: 'black', flex: 1 }}>Door 1</th>
-          {/* <th style={{ display: 'none' }}>Door 2</th> */}
-          <th style={{ background: 'lightgreen', color: 'black', flex: 2 }}>Door 3</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr style={{ textAlign: 'center', display: 'grid', gridTemplateColumns: '1fr 2fr' }}>
-          <td style={{ flex: 1 }}>Goat</td>
-          {/* <td style={{ display: 'none' }}>Goat</td> */}
-          <td style={{ flex: 2 }}>Car</td>
-        </tr>
-      </tbody>
-    </table>
-  );
-};
-
-const OddsColors = () => {
+const Table4 = () => {
   return (
     <table style={{ width: '100%', outline: 'none', borderSpacing: '0px' }}>
       <thead>
@@ -444,49 +278,26 @@ const OddsColors = () => {
   );
 };
 
-const TableHead = () => {
+const Table5 = () => {
   return (
-    <thead>
-      <tr>
-        <th>Door 1</th>
-        <th>Door 2</th>
-        <th>Door 3</th>
-      </tr>
-    </thead>
+    <table style={{ width: '100%' }}>
+      <thead>
+        <tr style={{ display: 'grid', gridTemplateColumns: '1fr 2fr' }}>
+          <th style={{ background: 'lightblue', color: 'black', flex: 1 }}>Door 1</th>
+          <th style={{ background: 'lightgreen', color: 'black', flex: 2 }}>Door 3</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr style={{ textAlign: 'center', display: 'grid', gridTemplateColumns: '1fr 2fr' }}>
+          <td style={{ flex: 1 }}>Goat</td>
+          <td style={{ flex: 2 }}>Car</td>
+        </tr>
+      </tbody>
+    </table>
   );
 };
 
-const Option1 = () => {
-  return (
-    <tr style={{ textAlign: 'center' }}>
-      <td>Goat</td>
-      <td>Goat</td>
-      <td>Car</td>
-    </tr>
-  );
-};
-
-const Option2 = () => {
-  return (
-    <tr>
-      <td>Goat</td>
-      <td>Car</td>
-      <td>Goat</td>
-    </tr>
-  );
-};
-
-const Option3 = () => {
-  return (
-    <tr>
-      <td>Car</td>
-      <td>Goat</td>
-      <td>Goat</td>
-    </tr>
-  );
-};
-
-const Breakdown = () => {
+const Table6 = () => {
   return (
     <table style={{ width: '100%' }}>
       <thead>
@@ -600,10 +411,6 @@ const PrizeInDoor3 = () => {
     </>
   );
 };
-
-const FluidText = styled.th`
-  /* font-size: clamp(0.5rem, 0.65rem + 0.1vw, 0.75rem); */
-`;
 
 const Th = styled.th`
   background: #915a22;
