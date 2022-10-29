80b581e9e717151ed8305ffb89c104931bb59c37
Matt Brannon <mattbrannon.dev@gmail.com>
2022-10-29 16:17:33 -0500

    refactor

--- components/Links/FancyLink.js
+++ components/Links/FancyLink.js
@@ -1,55 +1,32 @@
-import { InternalLink } from './InternalLink';
-// import { useEffect, useState } from 'react';
 import styled from 'styled-components';
-// import { Anchor } from './Anchor';
-import { memo } from 'react';
+import NextLink from 'next/link';
 
-// eslint-disable-next-line react/display-name
-export const FancyLink = memo(({ children, ...props }) => {
-  return (
-    <LinkWrapper {...props}>
-      <A {...props}>{children}</A>
-      <Underline />
-    </LinkWrapper>
-  );
-});
-
-const LinkWrapper = styled.span`
-  position: relative;
-  background: transparent;
-  border: none;
-  font-family: Jost, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
-    'OpenSans', 'Helvetica Neue', sans-serif;
+export const HeaderLink = styled(NextLink)`
+  color: var(--color);
+  display: block;
+  letter-spacing: 0.8px;
+  font-family: Jost;
   font-weight: 525;
 
-  --maxWidth: ${(p) => (p.$isActive ? '100%' : '0%')};
-  --color: ${(p) => (p.$isActive ? '#f6e9d5' : '#ffffff')};
-
   &:hover {
-    --maxWidth: 100% !important;
-  }
-`;
-
-const A = styled(InternalLink)`
-  color: var(--color);
-
-  letter-spacing: 0.8px;
-  text-decoration: none;
-  &:hover {
-    text-decoration: none;
     color: var(--off-white);
     cursor: pointer;
   }
   color: white;
   font-size: clamp(0.85rem, 0.85rem + 0.25vw, 1rem);
-  transition: color 0.15s ease;
+
+  /* border-bottom-color: ${(p) => (p.$isActive ? ' var(--fancy-link-underline)' : 'transparent')};
+  border-bottom-style: solid;
+  border-bottom-width: 2px; */
+
+  /* transition: color 0.15s ease, border-bottom-color 0.2s ease; */
 `;
 
 const Underline = styled.span`
   height: 2px;
   width: 100%;
 
-  max-width: var(--maxWidth, var(--max));
+  max-width: ${(p) => (p.isActive ? '100%' : '0%')};
   background: var(--fancy-link-underline);
   position: absolute;
   bottom: -4px;
@@ -60,3 +37,16 @@ const Underline = styled.span`
 
   transition: max-width 0.2s ease-in-out;
 `;
+
+const Wrapper = styled.div`
+  position: relative;
+`;
+
+export const FancyLink = ({ children, ...props }) => {
+  return (
+    <Wrapper>
+      <HeaderLink {...props}>{children}</HeaderLink>
+      <Underline {...props} />
+    </Wrapper>
+  );
+};
