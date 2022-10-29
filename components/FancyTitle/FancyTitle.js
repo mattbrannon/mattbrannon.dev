06edc476e6e36bdae5d03c483fcb29c100878691
Matt Brannon <mattbrannon.dev@gmail.com>
2022-10-29 13:06:12 -0500

    reduce complexity

--- components/FancyTitle/FancyTitle.js
+++ components/FancyTitle/FancyTitle.js
@@ -1,8 +1,4 @@
-import { m as motion } from 'framer-motion';
-import { useRef, Children } from 'react';
-import ClientOnly from '@components/ClientOnly';
-// import { Heading, Span } from './styles';
-import { Container, GradientSpan, Wrapper, TextAreaBox, OutlineText } from './styles';
+import { Container, GradientSpan, OutlineText } from './styles';
 
 export const FancyTitle = ({ children, ...props }) => {
   return (
@@ -12,25 +8,3 @@ export const FancyTitle = ({ children, ...props }) => {
     </Container>
   );
 };
-
-// export const FancyTitle = ({ children, ...props }) => {
-//   const container = useRef();
-
-//   return (
-//     <ClientOnly>
-//       <motion.div {...props}>
-//         <Heading {...props} ref={container} tabIndex={-1}>
-//           {Children.toArray(children).map((word, i, collection) => {
-//             const space = i < collection.length - 1 ? '\u{00a0}' : ' ';
-//             return (
-//               <Span key={i} {...props}>
-//                 {word}
-//                 {space}
-//               </Span>
-//             );
-//           })}
-//         </Heading>
-//       </motion.div>
-//     </ClientOnly>
-//   );
-// };
