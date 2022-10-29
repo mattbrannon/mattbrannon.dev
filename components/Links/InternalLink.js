af6899977fbceb771b8fc9f0418b0d284f166c49
Matt Brannon <mattbrannon.dev@gmail.com>
2022-10-29 16:18:04 -0500

    refactor

--- components/Links/InternalLink.js
+++ components/Links/InternalLink.js
@@ -1,25 +1,9 @@
 import NextLink from 'next/link';
 import styled from 'styled-components';
-import { Anchor } from './Anchor';
-import { forwardRef } from 'react';
 
-function Link({ href, children, ...props }, ref) {
-  return (
-    <NextLink {...props} passHref href={href} legacyBehavior>
-      <Internal ref={ref} {...props}>
-        {children}
-      </Internal>
-    </NextLink>
-  );
-}
-
-export const InternalLink = forwardRef(Link);
-
-const Internal = styled(Anchor)`
+export const InternalLink = styled(NextLink)`
   color: var(--internal-link);
-  /* font-weight: 700; */
   font: inherit;
-  /* font-variation-settings: 'wght' 750; */
 
   &:hover {
     color: var(--internal-link-hover);
